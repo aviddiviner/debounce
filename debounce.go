110a806a33309bcd5d260b154e36c8a8ecc7c64c
David Irvine <aviddiviner@gmail.com>
2017-09-12 19:09:46 -0500

    Add command line args and usage

--- debounce.go
+++ debounce.go
@@ -6,7 +6,27 @@ import (
 	"io"
 	"os"
 	"time"
+
+	"github.com/aviddiviner/docopt-go"
 )
+
+const usage = `Tail lines from STDIN and output the last line to STDOUT, after receiving
+nothing for a while.
+
+Usage:
+  debounce [options]
+
+Example:
+  tail -F * | debounce -d 100
+  yes | head -50000 | debounce
+
+Options:
+  -d <ms>       Delay in milliseconds [default: 500].
+  -h --help     Show this screen.`
+
+type Options struct {
+	Delay int `docopt:"-d"`
+}
 
 func writeLine(b []byte, out io.Writer) (err error) {
 	if b == nil {
@@ -64,6 +84,17 @@ func debounce(src io.Reader, dst io.Writer, errOut io.Writer, delay time.Duratio
 }
 
 func main() {
-	delay := 500 * time.Millisecond
+	checkErr := func(err error) {
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error parsing command line args:", err)
+			os.Exit(1)
+		}
+	}
+	var opts Options
+	args, err := docopt.ParseDoc(usage)
+	checkErr(err)
+	checkErr(args.Bind(&opts))
+
+	delay := time.Duration(opts.Delay) * time.Millisecond
 	debounce(os.Stdin, os.Stdout, os.Stderr, delay)
 }
